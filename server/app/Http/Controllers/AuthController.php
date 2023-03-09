ff6c0187efd7da8a5785c61cd7d9411898ef2b6f
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-03-09 09:18:55 +0100

    Added conditional on register funciton

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -3,23 +3,34 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
 use Illuminate\Http\Request;
-use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
 {
 	public function register(RegisterRequest $request)
 	{
 		$data = $request->validated();
-
-		/** @var \App\Models\User $user */
-		$user = User::create([
-			'name' => $data['name'],
-			'email' => $data['email'],
-			'password' => bcrypt($data['password']),
-		]);
+		if ($data['isCompany'] === false) {
+			$user = User::create([
+				'name' => $data['name'],
+				'surname' => $data['surname'],
+				'email' => $data['email'],
+				'city' => $data['city'],
+				'password' => bcrypt($data['password']),
+				'isCompany' => $data['isCompany']
+			]);
+		} else {
+			$user = User::create([
+				'name' => $data['name'],
+				'email' => $data['email'],
+				'city' => $data['city'],
+				'password' => bcrypt($data['password']),
+				'isCompany' => $data['isCompany']
+			]);
+		}
 
 		$token = $user->createToken('main')->plainTextToken;
 
