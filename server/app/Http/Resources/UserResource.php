c23de02b9a243692c00e1e0ea39ed9a3f436ee05
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-03-09 09:19:27 +0100

    Added UserResource

--- server/app/Http/Resources/UserResource.php
+++ server/app/Http/Resources/UserResource.php
@@ -7,13 +7,20 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(Request $request): array
-    {
-        return parent::toArray($request);
-    }
+	/**
+	 * Transform the resource into an array.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function toArray(Request $request): array
+	{
+		return [
+			'id' => $this->id,
+			'name' => $this->name,
+			'surname' => $this->surname,
+			'email' => $this->email,
+			'city' => $this->city,
+			'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+		];
+	}
 }
